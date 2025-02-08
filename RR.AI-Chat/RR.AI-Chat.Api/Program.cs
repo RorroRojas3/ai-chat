3f66c8503b6364045b22071dd178d3f849a3bb4c
Rodrigo Rojas <rorro.irg@gmail.com>
2025-02-08 18:44:48 -0500

    Update OllamaChatClient URI to remote server
    The URI for the `OllamaChatClient` in the `AddChatClient` method has been updated from `http://localhost:11434/` to `https://a46d-136-55-186-105.ngrok-free.app/`. This change suggests a transition from a local server to a remote server, possibly for testing or deployment purposes.

--- RR.AI-Chat/RR.AI-Chat.Api/Program.cs
+++ RR.AI-Chat/RR.AI-Chat.Api/Program.cs
@@ -19,7 +19,8 @@ builder.Services.AddCors(builder => builder.AddPolicy("AllowSpecificOrigins", po
 }));
 
 /// AI Chat clients
-builder.Services.AddChatClient(new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.2"));
+var ollamaUrl = builder.Configuration.GetValue<string>("OllamaUrl");
+builder.Services.AddChatClient(new OllamaChatClient(new Uri(ollamaUrl ?? "http://localhost:11434/"), "llama3.2"));
 
 // AI Embedding Generators
 IEmbeddingGenerator<string, Embedding<float>> ollamaGenerator =
